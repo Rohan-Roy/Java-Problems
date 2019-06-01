f1bd084d13de0c9fd78a0e96e3813389eb99941b
Rohan Roy <43656700+Rohan-Roy@users.noreply.github.com>
2019-06-01 13:46:20 -0700

    Create LeftViewOfTree.java
    Left view of a tree.

---
+++ LeftViewOfTree.java
@@ -0,0 +1,29 @@
+class LeftViewOfTree
+{
+    static int maxLevel = 0;
+    void leftView(Node root)
+    {
+        if(root!=null)
+        leftView(root, 1);
+        maxLevel = 0;
+        return;
+      // Your code here
+    }
+    
+    void leftView( Node root, int level){
+        if(root!=null && maxLevel < level)
+        System.out.print(root.data+" ");
+        if(level>maxLevel)
+        maxLevel = level;
+
+        if(root.left!=null){
+            level++;
+            leftView(root.left, level);
+        }
+        level--;
+        if(root.right!=null){
+            level++;
+            leftView(root.right, level);
+        }
+    }
+}
